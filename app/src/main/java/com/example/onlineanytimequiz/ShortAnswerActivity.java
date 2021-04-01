b6957c3e1bf7f7e1f53626c4d37262b56f77249d
Sophie0627 <bolesalavb@gmail.com>
2021-04-01 16:57:19 +0900

    connect short answer activity

--- app/src/main/java/com/example/onlineanytimequiz/ShortAnswerActivity.java
+++ app/src/main/java/com/example/onlineanytimequiz/ShortAnswerActivity.java
@@ -2,14 +2,11 @@ package com.example.onlineanytimequiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class ShortAnswerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
 
-        Button btn = (Button)findViewById(R.id.main_continue_btn);
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, ShortAnswerActivity.class));
-            }
-        });
+        setContentView(R.layout.activity_short_answer);
     }
-
 }
