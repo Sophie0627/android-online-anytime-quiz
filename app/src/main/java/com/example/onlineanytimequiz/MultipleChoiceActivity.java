6fb1934b9b416e248e1835c43e0d77952db12465
Sophie0627 <bolesalavb@gmail.com>
2021-04-02 02:01:09 +0900

    build multi choice activity ui

--- app/src/main/java/com/example/onlineanytimequiz/MultipleChoiceActivity.java
+++ app/src/main/java/com/example/onlineanytimequiz/MultipleChoiceActivity.java
@@ -2,14 +2,11 @@ package com.example.onlineanytimequiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
 
-public class ShortAnswerActivity extends AppCompatActivity {
+public class MultipleChoiceActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,15 +22,6 @@ public class ShortAnswerActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        setContentView(R.layout.activity_short_answer);
-
-        Button btn = (Button)findViewById(R.id.short_answer_continue_btn);
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(ShortAnswerActivity.this, MultipleChoiceActivity.class));
-            }
-        });
+        setContentView(R.layout.activity_multiple_choice);
     }
 }
